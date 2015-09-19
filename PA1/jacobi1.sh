871cd26ca36660e59c82c2c599b2c265547c4efe
Corey Lefevre <cjlefevr@juneau.cs.colostate.edu>
2015-09-19 01:48:00 -0600

    Finished PA1 Program

--- PA1/jacobi1.sh
+++ PA1/jacobi1.sh
@@ -1,23 +1,25 @@
-
 num_threads=1 schedule=
 
-while getopts t:s: opt; do
+while getopts t: opt; do
   case $opt in
   t)
       num_threads=$OPTARG
       ;;
-  s)
-      schedule=$OPTARG
-      ;;
+
+#  s)
+#      schedule=$OPTARG
+#      ;;
   esac
 done
 
 shift $((OPTIND - 1))
 
+make jacobi_1D
+
 while [ $num_threads -lt 9 ]; do  
   export OMP_NUM_THREADS=$num_threads
-  export OMP_SCHEDULE=$schedule
-  echo $schedule
+#  export OMP_SCHEDULE=$schedule
+#  echo $schedule
   echo $num_threads
   for run in {1..5}
   do
--- PA1/matvec.sh
+++ PA1/matvec.sh
@@ -1,27 +1,29 @@
 
 num_threads=1 schedule=
 
-while getopts t:s: opt; do
+while getopts t: opt; do
   case $opt in
   t)
       num_threads=$OPTARG
       ;;
-  s)
-      schedule=$OPTARG
-      ;;
+#  s)
+#      schedule=$OPTARG
+#      ;;
   esac
 done
 
 shift $((OPTIND - 1))
 
+make mat_vec
+
 while [ $num_threads -lt 9 ]; do  
   export OMP_NUM_THREADS=$num_threads
-  export OMP_SCHEDULE=$schedule
-  echo $schedule
+#  export OMP_SCHEDULE=$schedule
+#  echo $schedule
   echo $num_threads
   for run in {1..5}
   do
-    ./mat_vec 15000 10000
+    ./mat_vec 25000 10000
   done
 let num_threads=num_threads+1
 done
